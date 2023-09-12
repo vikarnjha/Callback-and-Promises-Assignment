070456cbea13edad500d0a2d3862253cd03f96e6
Vikarn Jha <vikarnjha91@gmail.com>
2023-09-12 23:55:12 +0530

    Q1 function oriantation changed

--- 1-DoubleUsingCallback.js
+++ 1-DoubleUsingCallback.js
@@ -1,13 +1,13 @@
-function callback(num) {
-    return num*2;
-}
 function doubleArray(arr, callback) {
-    const doubleArray = arr.map((num) => {
-        return callback(num);
-    })
-    return doubleArray;
+  const doubleArray = arr.map((num) => {
+    return callback(num);
+  });
+  return doubleArray;
 }
-const originalArray = [1,2,3,4,5];
-newArray = doubleArray(originalArray, callback)
+function callback(num) {
+  return num * 2;
+}
+const originalArray = [1, 2, 3, 4, 5];
+newArray = doubleArray(originalArray, callback);
 console.log(newArray); // [ 2, 4, 6, 8, 10 ]
-console.log(doubleArray(newArray, callback));  // [ 4, 8, 12, 16, 20 ]
+console.log(doubleArray(newArray, callback)); // [ 4, 8, 12, 16, 20 ]
