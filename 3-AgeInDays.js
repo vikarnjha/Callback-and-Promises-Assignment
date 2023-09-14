6a77a05ed6d5bef374d10b2d610b2cf3339c277c
Vikarn Jha <vikarnjha91@gmail.com>
2023-09-14 23:52:11 +0530

    Q3 completed

---
+++ 3-AgeInDays.js
@@ -0,0 +1,16 @@
+const person = {
+    firstName: "Vikarn",
+    lastName: "Jha",
+    age: 20,
+};
+
+function ageInDays(person, callback){
+    const fullName = `${person.firstName} ${person.lastName}`
+    const ageInDays = person.age*365;
+    callback(fullName, ageInDays);
+}
+
+function logResult(fullName, ageInDays) {
+    console.log(`The person's full name is ${fullName} and their age in days is ${ageInDays}`);
+}
+ageInDays(person, logResult);
